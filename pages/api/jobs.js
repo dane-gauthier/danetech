b155a8bfdaee281bd4d59f39f614c1f520a23cdd
Dane Gauthier <danegauthier@gmail.com>
2023-06-24 03:06:19 -0400

    Data purge

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -1,52 +1,7 @@
 export const jobs = [
   {
-    position: 'scrum master / devops consultant',
-    company: 'CGI Federal',
-    duration: 'august 2022 - present',
-    duties: [
-      '- conduct project management for the Momentum Apprenticeship Program facilitating, organizing, and restructuring trainings, panels, and various learning opportunities.',
-      '- provide constant feedback through reviews, daily stand-ups, retrospectives, and metrics',
-      '- successfully implement solutions when bottlenecks in processes are found by communicating metrics to leaders to unblock developers and trainees'
-    ]
-  },
-  {
-    position: 'software developer',
-    company: 'turner industries',
-    duration: 'september 2021 - june 2022',
-    duties: [
-      '- refactored and released an internal legacy API management tool independently in less than 3 months',
-      '- successfully passed the Professional Scrum Master exam while contributing to frontend applications, reviewing PRs, and creating company coding standards documentation',
-      '- established a daily code-sharing habit among team members by utilizing IDE tools, planning daily pair-programming sessions, and communicating feedback'
-    ]
-  },
-  {
-    position: 'associate technical consultant',
-    company: 'perficient, inc',
-    duration: 'january 2021 - july 2021',
-    duties: [
-      '- generated up to 5 highly detailed data visualizations outlining web performance to present to high priority client',
-      '- updated up to 20 processes using XLRelease to create higher efficiency within DevOps practice',
-      '- created several documents outlining release processes and explaining DevOps practice using Confluence'
-    ]
-  },
-  {
-    position: 'software developer',
-    company: 'gophr, inc',
-    duration: 'september 2019 - july 2020',
-    duties: [
-      '- released first iteration of internal MVP web application in less than 2 months with a team of less than 10 developers',
-      '- built and released the first iteration of MVP mobile application after 2 weeks of React Native courses',
-      '- refactored and released external MVP web application and educated team on current JavaScript and React standards for successful future refactor and development'
-    ]
-  },
-  {
-    position: 'junior web developer',
-    company: 'waitr, inc',
-    duration: 'march 2019 - september 2019',
-    duties: [
-      '- built and released the MVP of Waitr Impact, a social justice initiative with little supervision in less than 6 months immediately after boot camp completion',
-      '- collaborated with senior developer to successfully resolve bugs, add features, and improve design throughout external web platform',
-      '- documented and updated over 100 issues using JIRA and Confluence in less than 2 weeks which significantly decreased backlog refinement time for product owner and department'
-    ]
+    position: '',
+    company: '',
+    duration: '',
+    duties: [    ]
   }
-]
